f8406c8ca85a72e57c414fe2d66ef6e627dddfaf
Jared Jenvey <jared@exit-12.com>
2023-07-21 02:42:44 +0000

    Replaced ternary operator with NOT operator

--- react-state/my-app/src/ToggleButton.js
+++ react-state/my-app/src/ToggleButton.js
@@ -7,7 +7,7 @@ export default function ToggleButton({ text, color }) {
 
   const handleClick = () => {
     console.log('before setter', isClicked);
-    setIsClicked(isClicked ? false : true);
+    setIsClicked(!isClicked);
     console.log('after setter', isClicked);
   };
 
